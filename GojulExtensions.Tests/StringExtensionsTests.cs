8494f9e994e5f08bfead895f2dfe71e990646b41
Julien AUBIN <julien_aubin_lm@yahoo.fr>
2018-12-12 18:56:20 +0100

    Added string extension class

---
+++ GojulExtensions.Tests/StringExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xunit;
+
+namespace Org.Gojul.Extensions.Tests
+{
+    public enum Foo
+    {
+        Foo, Bar
+    }
+
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void TestParseEnumWithNullStringThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() => StringExtensions.ParseEnum<Foo>(null));
+        }
+
+        [Fact]
+        public void TestParseEnum()
+        {
+            Assert.Equal(Foo.Foo, "Foo".ParseEnum<Foo>());
+            Assert.Equal(Foo.Bar, "bar".ParseEnum<Foo>());
+            Assert.Throws<ArgumentException>(() => "linux".ParseEnum<Foo>());
+        }
+    }
+}
---
+++ GojulExtensions/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Conditions;
+
+namespace Org.Gojul.Extensions
+{
+    /// <summary>
+    /// Class <code>StringExtensions</code> contains
+    /// various useful methods to be used with strings.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Convert the string <code>value</code> into an enum type. Note
+        /// that this method is not case sensitive.
+        /// </summary>
+        /// <typeparam name="T">the type of the enum to parse</typeparam>
+        /// <param name="value">the value to parse</param>
+        /// <returns>the enum value corresponding to <code>value</code>.</returns>
+        /// <exception cref="ArgumentNullException">if <code>value</code> is <code>null</code>.</exception>
+        /// <exception cref="ArgumentException">if <code>value</code> does not match a value of the specified enum.</exception>
+        public static T ParseEnum<T>(this string value) where T : struct
+        {
+            // This method is inspired by :
+            // https://stackoverflow.com/questions/32542356/how-to-save-enum-in-database-as-string
+            Condition.Requires(value).IsNotNull();
+            return (T)Enum.Parse(typeof(T), value, true);
+        }
+    }
+}
